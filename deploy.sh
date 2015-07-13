2ac1e9defc2e72e14641ae2b73a4ea2db0a3f34c
Thomas Thoren <thomasjthoren@gmail.com>
2015-07-13 17:36:08 -0500

    Pruning.

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-aws s3 cp index.html s3://lensnola/salaryexplorer/index.html --acl public-read
-aws s3 cp js/ s3://lensnola/salaryexplorer/js/ --acl public-read --recursive --content-type text/plain
-aws s3 cp css/ s3://lensnola/salaryexplorer/css/ --acl public-read --recursive --content-type text/plain
-aws s3 cp data/ s3://lensnola/salaryexplorer/data/ --acl public-read --recursive --content-type text/plain
